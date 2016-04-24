c419376177e9de9b4df6bd61bac28042c27cab77
Quinntyne Brown <quinntynebrown@gmail.com>
2016-04-24 16:33:13 -0400

    use owincontext

--- Server/Middleware/TenantMiddleware.cs
+++ Server/Middleware/TenantMiddleware.cs
@@ -4,6 +4,7 @@ using Microsoft.Practices.Unity;
 using static Chloe.Server.UnityConfiguration;
 using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
 using Chloe.Server.Services.Contracts;
+using Microsoft.Owin;
 
 namespace Chloe.Server.Middleware
 {   
@@ -16,11 +17,11 @@ namespace Chloe.Server.Middleware
             _next = next;
         }
 
-        public async Task Invoke(IDictionary<string, object> environment)
-        {
-            var tenant = GetContainer().Resolve<ITenantService>().GetByUri((string)environment["Request.Uri.Host"]);
-            environment.Add("MultiTenant", tenant);
-            await _next.Invoke(environment);
+        public async Task Invoke(IDictionary<string, object> env)
+        {            
+            var tenant = GetContainer().Resolve<ITenantService>().GetByUri(new OwinContext(env).Request.Uri.Host);
+            env.Add("MultiTenant", tenant);
+            await _next.Invoke(env);
         }
     }
 }
